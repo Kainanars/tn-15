c493a20d46417ed48d134b4eab92c2323a6f31aa
Kainan Rodrigues <kainan.ars@gmail.com>
2020-12-02 00:07:16 -0300

    Alteração no console.log

--- ageVerifyc.js
+++ ageVerifyc.js
@@ -11,15 +11,15 @@ var adulto = [];
 for (var i=0 ; i<idade.length; i++) {
     if (idade[i] <=10){
         crianca.push(idade[i]);
-        console.log("É uma criança")
+        console.log("É uma criança! Pois tem " + idade[i] + " anos.")
     }
     else if ((idade[i]>=11) && (idade[i]<=17)){
         adolescente.push(idade[i]);
-        console.log("É um adolescente")
+        console.log("É um adolescente! Pois tem " + idade[i] + " anos.")
     }
     else if (idade[i]>=18){
         adulto.push(idade[i]);
-        console.log("É um adulto")
+        console.log("É um adulto! Pois tem " + idade[i] + " anos.")
     }
 }
 
