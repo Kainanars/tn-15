5e4e5ffc011b76749d2635c4fb2ce8ba60f5dfc7
Kainan Rodrigues - Escritório <kainan.ars@gmail.com>
2020-12-02 12:16:41 -0300

    Solucionando problema do calculo de aliquota

--- calculeINSS.js
+++ calculeINSS.js
@@ -10,24 +10,24 @@ function calculeINSS (salario){
         console.log(`Sálario: ${salario}, Aliquota: ${formatter.format(aliquota)}, Valor INSS: ${formatter.format(valorINSS)}`);
         return;
     }
-    else if (salario >= 1045.01 <= 2089.60){
+    else if (salario >= 1045.01 && salario <=2089.60){
         const valorINSS = ((salario - 1045.00) * 0.09) + 78.37;
         const aliquota = (valorINSS / salario) * 100;
         console.log(`Sálario: ${salario}, Aliquota: ${formatter.format(aliquota)}, Valor INSS: ${formatter.format(valorINSS)}`);
         return;
     }
-    else if (salario >= 2089.61 <= 3134.40){
+    else if (salario >= 2089.61 && salario <= 3134.40){
         const valorINSS = ((salario - 2089.60) * 0.12) + 172.38;
         const aliquota = (valorINSS / salario) * 100;
         console.log(`Sálario: ${salario}, Aliquota: ${formatter.format(aliquota)}, Valor INSS: ${formatter.format(valorINSS)}`);
         return;
     }
     else {
-        const valorINSS = ((salario - 3134.40) * 0.14) + 297.75;
+        const valorINSS = ((salario - 3134.40) * 0.14) + 297.75 + 172.38;
         const aliquota = (valorINSS / salario) * 100;
         console.log(`Sálario: ${salario}, Aliquota: ${formatter.format(aliquota)}, Valor INSS: ${formatter.format(valorINSS)}`);
         return;
     }
 }
 
-calculeINSS(1200);
+calculeINSS(3000.00);
